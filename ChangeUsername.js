02ee5702efae5768e688afbce196ca39a1ab4852
J. Degand <70610011+jdegand@users.noreply.github.com>
2023-05-25 13:32:49 -0400

    Update ChangeUsername.js

--- ChangeUsername.js
+++ ChangeUsername.js
@@ -1,38 +1,44 @@
 // React is loaded and is available as React and ReactDOM
 // imports should NOT be used
 class Username extends React.Component {
-    state = { value: "" };
-  
-    changeValue(value) {
-      this.setState({ value });
-    }
-  
-    render() {
-      const { value } = this.state;
-      return <h1>{value}</h1>;
-    }
-  }
-  
-  function App(props) {
-    const nameRef = React.useRef(null);
+  state = { value: "" };
 
-    function clickHandler() {
-      document.querySelector("h1").innerHTML = nameRef.current.value;
-    }
-  
-    return (
-      <div>
-        <button onClick={clickHandler}>Change Username</button>
-        <input type="text" ref={nameRef} />
-        <Username  />
-      </div>
-    );
+  changeValue(value) {
+    this.setState({ value });
   }
+
+  render() {
+    const { value } = this.state;
+    return <h1>{value}</h1>;
+  }
+}
+
+function App() {
+  const nameRef = React.useRef();
+  const userNameRef = React.useRef();
   
-  document.body.innerHTML = "<div id='root'></div>";
-  const rootElement = document.getElementById("root");
-  ReactDOM.render(<App />, rootElement);
-  
+  function clickHandler() {
+     userNameRef.current.setState({ value: nameRef.current.value });
+  }
+
+  return (
+    <div>
+      <button onClick={clickHandler}>Change Username</button>
+      <input type="text" ref={nameRef} />
+      <Username ref={userNameRef} />
+    </div>
+  );
+}
+
+document.body.innerHTML = "<div id='root'></div>";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+
+setTimeout(() => {
   document.querySelector("input").value = "John Doe";
   document.querySelector("button").click();
-  setTimeout(() => console.log(document.getElementById("root").innerHTML));
+
+  setTimeout(() => {
+    console.log(document.getElementById("root").innerHTML);
+  }, 300);
+}, 300);
