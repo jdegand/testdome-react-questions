7b70a3249da498404ac1d613d46e19ab274af661
jdegand <70610011+jdegand@users.noreply.github.com>
2023-12-21 16:02:02 -0500

    ImageGalleryApp updated to react 18

--- ImageGalleryApp.js
+++ ImageGalleryApp.js
@@ -1,37 +1,32 @@
-// React is loaded and is available as React and ReactDOM
-// imports should NOT be used
-class ImageGallery extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      links: props.links
-    }
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const ImageGallery = ({ links }) => {
+  const remove = (event) => {
+    event.target.parentElement.remove();
   }
-  
-  remove(event){
-    event.target.closest('div.image').remove();
-  }
-  
-  
-  render() {
-    return (
+
+  return (
     <div>
-      {this.state.links.map((link, index) => <div key={index} class="image">
-                              <img src={link} />
-                                <button class="remove" onClick={event => this.remove(event)}>X</button>
-                              </div>
-                                )}  
+      {links.map((link, index) => <div key={index} class="image">
+        <img src={link} />
+        <button class="remove" onClick={event => remove(event)}>X</button>
+      </div>
+      )}
     </div>
-      )
-  }
-      
+  );
 }
 
 document.body.innerHTML = "<div id='root'> </div>";
-  
+
 const rootElement = document.getElementById("root");
-const links = ["https://goo.gl/kjzfbE", "https://goo.gl/d2JncW"];
-ReactDOM.render(<ImageGallery links={ links } />,
-                rootElement);
-document.querySelectorAll('.remove')[0].click();
-console.log(rootElement.innerHTML);
+const links = ["https://bit.ly/3lmYVna", "https://bit.ly/3flyaMj"];
+const root = createRoot(rootElement);
+root.render(<ImageGallery links={links} />);
+
+setTimeout(() => {
+  document.querySelectorAll('.remove')[0]?.click();
+  setTimeout(() => {
+    console.log(rootElement?.innerHTML);
+  });
+});
